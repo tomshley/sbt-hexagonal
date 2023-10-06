f22c1b85e1f626109d4f6963865a631b266cd17c
sgoggles <gitlab.comsgogglespm.coil197@passmail.net>
2023-10-06 09:45:38 -0400

    WIP tomshley/brands/global/tware/tech/products/hexagonal/hexagonal-plugins-sbt#1
    https://gitlab.com/tomshley/brands/global/tware/tech/products/hexagonal/hexagonal-plugins-sbt/-/issues/1

---
+++ common/src/main/scala/com/tomshley/brands/global/tech/tware/products/hexagonal/plugins/common/model/HexagonalArchitectureModel.scala
@@ -0,0 +1,23 @@
+package com.tomshley.brands.global.tech.tware.products.hexagonal.plugins.common.model
+
+sealed trait HexagonalPart {}
+
+sealed trait HexagonalOuter extends HexagonalPart {}
+
+sealed trait HexagonalInfra extends HexagonalPart {}
+
+sealed trait HexagonalLayer extends HexagonalPart {}
+
+case object LoggingAdapter extends HexagonalOuter
+
+case object StreamingAdapter extends HexagonalInfra
+
+case object TransactionalAdapter extends HexagonalInfra
+
+case object ObjectPersistenceAdapter extends HexagonalInfra
+
+case object Edge extends HexagonalLayer
+
+case object ValueAdd extends HexagonalLayer
+
+case object Core extends HexagonalLayer
---
+++ hexagonal-plugin-projectstructure/src/main/scala/com/tomshley/brands/global/tech/tware/products/hexagonal/plugins/projectstructure/FilesystemArtifacts.scala
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2022. Tomshley Llc. All Rights Reserved.
+ * @author Thomas Schena @sgoggles <https://github.com/sgoggles>
+ */
+
+package com.tomshley.brands.global.tech.tware.products.hexagonal.plugins.projectstructure
+
+import com.tomshley.brands.global.tech.tware.products.hexagonal.plugins.common.utility.PackageNameOperations.{appNameToPackageName, packageNameToFilepath}
+import sbt.*
+
+/**
+ *
+ */
+protected[projectstructure] class FilesystemArtifacts(val fqdnPath: String) extends FilesystemArtifactsDef
+
+protected[projectstructure] object FilesystemArtifacts {
+  def apply(
+             projectName:String,
+             orgSpace:String,
+             dir: Option[File]): Unit = {
+
+    lazy val src = dir.getOrElse(file(".")).getAbsolutePath + "/src"
+    lazy val srcMain = src + "/main"
+    lazy val srcMainScala = srcMain + "/scala"
+    lazy val fqdnPath = srcMainScala + "/" + packageNameToFilepath(
+      Seq(
+        orgSpace,
+        appNameToPackageName(projectName)
+      ).mkString(".")
+    )
+
+    new FilesystemArtifacts(fqdnPath).enforce()
+  }
+}
+
+/**
+ */
+sealed trait FilesystemArtifactsDef {
+  private[this] lazy val filePaths: List[String] = List(
+    fqdnPath,
+    fqdnPath + "/app",
+    fqdnPath + "/core",
+    fqdnPath + "/core/models",
+    fqdnPath + "/core/ports",
+    fqdnPath + "/core/ports/incoming",
+    fqdnPath + "/core/ports/outgoing",
+    fqdnPath + "/infrastructure",
+    fqdnPath + "/infrastructure/config",
+    fqdnPath + "/infrastructure/adapters",
+    fqdnPath + "/infrastructure/adapters"
+  )
+
+  private[this] lazy val files: List[File] = {
+    for (fp <- filePaths)
+      yield new File(fp)
+  }
+
+  private[this] lazy val readmeFiles: List[File] = {
+    for (fp <- filePaths)
+      yield new File(fp + "/README.md")
+  }
+  val fqdnPath: String
+
+  def enforce(): Unit = {
+    files.filter(!_.exists()).foreach(f => {
+
+      f.mkdirs()
+    })
+
+    readmeFiles.filter(!_.exists()).foreach(f => {
+      lazy val readmeParent = f.getParentFile
+
+      if (readmeParent.isDirectory) {
+        lazy val readmeSiblingsNoExist = readmeParent.listFiles().forall(_.isDirectory)
+
+        if (readmeSiblingsNoExist) {
+          f.createNewFile()
+        }
+      }
+    })
+  }
+
+  override def toString: String = {
+    filePaths.toString()
+  }
+}
+
