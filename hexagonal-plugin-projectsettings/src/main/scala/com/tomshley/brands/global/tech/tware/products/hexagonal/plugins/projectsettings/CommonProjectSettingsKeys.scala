7437a8908596804aee66d5f235acb85b330af9e9
sgoggles <gitlab.comsgogglespm.coil197@passmail.net>
2023-10-25 09:47:56 -0400

    WIP tomshley/brands/global/tware/tech/products/hexagonal/hexagonal-plugins-sbt#1
    https://gitlab.com/tomshley/brands/global/tware/tech/products/hexagonal/hexagonal-plugins-sbt/-/issues/1

--- hexagonal-plugin-projectsettings/src/main/scala/com/tomshley/brands/global/tech/tware/products/hexagonal/plugins/projectsettings/CommonProjectSettingsKeys.scala
+++ hexagonal-plugin-projectsettings/src/main/scala/com/tomshley/brands/global/tech/tware/products/hexagonal/plugins/projectsettings/CommonProjectSettingsKeys.scala
@@ -20,15 +20,21 @@
 package com.tomshley.brands.global.tech.tware.products.hexagonal.plugins.projectsettings
 
 import com.tomshley.brands.global.tech.tware.products.hexagonal.plugins.common.sbt.BasicSbtSettingsKeys
-import sbt.*
+import sbt.Keys.{licenses, scalaVersion, scalacOptions, version}
+import sbt.url
 
-protected[projectsettings] trait ProjectsHelperKeys extends BasicSbtSettingsKeys {
-
-  def internalProject(projectName: String, projectBaseOption: Option[File] = None): Project = {
-    ProjectSbtSettingsModel(projectName = projectName, projectBaseOption = projectBaseOption).sbtProject
-  }
-
-  def publishableProject(projectName: String, projectBaseOption: Option[File] = None): Project = {
-    ProjectSbtSettingsModel(projectName = projectName, projectBaseOption = projectBaseOption).sbtProject
-  }
+protected[projectsettings] trait CommonProjectSettingsKeys extends BasicSbtSettingsKeys {
+  lazy val baseSettings3: sbt.Def.SettingsDefinition = Seq(
+    licenses := {
+      val tagOrBranch =
+        if (version.value.endsWith("SNAPSHOT")) "main"
+        else "v" + version.value
+      Seq(
+        ("APACHE-2.0",
+          url("https://raw.githubusercontent.com/tomshley/hexagonal-plugins-sbt/" + tagOrBranch + "/LICENSE"))
+      )
+    },
+    scalacOptions += "-Wconf:cat=deprecation&msg=.*JavaConverters.*:s",
+    scalaVersion := ProjectSettingsDefs.Scala3
+  )
 }
