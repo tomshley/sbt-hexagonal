9563a640aafd8d898ae7fe286c61319c7d342676
sgoggles <gitlab.comsgogglespm.coil197@passmail.net>
2023-10-27 10:43:49 -0400

    WIP tomshley/brands/global/tware/tech/products/hexagonal/hexagonal-plugins-sbt#3
    https://gitlab.com/tomshley/brands/global/tware/tech/products/hexagonal/hexagonal-plugins-sbt/-/issues/3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,47 +8,56 @@ object Dependencies {
   val Scala2Versions: Seq[String] = Seq(Scala213, Scala212)
   val ScalaVersions: Seq[String] = Dependencies.Scala2Versions :+ Dependencies.Scala3
 
-  val commonProject: Seq[Def.Setting[Seq[ModuleID]]] = Seq(
+  val commonProject: Seq[Def.Setting[?]] = Seq(
     libraryDependencies ++= Seq(
 
     )
   )
-  val acceptancePluginProject: Seq[Def.Setting[Seq[ModuleID]]] = Seq(
+  val acceptancePluginProject: Seq[Def.Setting[?]] = Seq(
     libraryDependencies ++= Seq(
 
     )
   )
-  val cicdPluginProject: Seq[Def.Setting[Seq[ModuleID]]] = Seq(
+  val cicdPluginProject: Seq[Def.Setting[?]] = Seq(
     libraryDependencies ++= Seq(
 
     )
   )
-  val layeredPluginProject: Seq[Def.Setting[Seq[ModuleID]]] = Seq(
+  val layeredPluginProject: Seq[Def.Setting[?]] = Seq(
     libraryDependencies ++= Seq(
 
     )
   )
-  val libraryPluginProject: Seq[Def.Setting[Seq[ModuleID]]] = Seq(
+  val libraryPluginProject: Seq[Def.Setting[?]] = Seq(
     libraryDependencies ++= Seq(
 
     )
   )
-  val protobufsPluginProject: Seq[Def.Setting[Seq[ModuleID]]] = Seq(
+  val protobufsPluginProject: Seq[Def.Setting[?]] = Seq(
     libraryDependencies ++= Seq(
 
     )
   )
-  val settingsPluginProject: Seq[Def.Setting[Seq[ModuleID]]] = Seq(
+  val settingsPluginProject: Seq[Def.Setting[?]] = Seq(
     libraryDependencies ++= Seq(
 
     )
   )
-  val structuredPluginProject: Seq[Def.Setting[Seq[ModuleID]]] = Seq(
+  val structuredPluginProject: Seq[Def.Setting[?]] = Seq(
     libraryDependencies ++= Seq(
 
     )
   )
-  val templatePluginProject: Seq[Def.Setting[Seq[ModuleID]]] = Seq(
+  val templatePluginProject: Seq[Def.Setting[?]] = Seq(
+    Compile / unmanagedResourceDirectories := {
+      Seq(baseDirectory.value / "src/main/resources")
+    },
+//    Compile / target := {
+//      baseDirectory.value / "sbt-stuff"
+//    },
+//    Compile / classDirectory := {
+//      baseDirectory.value / "target"
+//    },
     libraryDependencies ++= Seq(
 
     )
